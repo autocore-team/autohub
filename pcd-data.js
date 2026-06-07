385efc8088fc56c9f8b43a845bacc92c50fd2325
autocore-team <loowdanny@gmail.com>
2026-06-07 20:41:58 +0200

    Create pcd-data.js

---
+++ pcd-data.js
@@ -0,0 +1,121 @@
+const PCD_DATA = [
+  {
+    name: 'Audi',
+    slug: 'audi',
+    models: [
+      {
+        name: 'A3',
+        slug: 'a3',
+        records: [
+          {
+            generation: '8L',
+            years: '1996-2003',
+            market: 'EU',
+            pcd: '5x100',
+            centerBore: '57.1',
+            thread: 'M14x1.5',
+            fastener: 'bolts',
+            offset: 'ET32-42',
+            status: 'needs_review'
+          },
+          {
+            generation: '8P / 8V',
+            years: '2003-2020',
+            market: 'EU',
+            pcd: '5x112',
+            centerBore: '57.1',
+            thread: 'M14x1.5',
+            fastener: 'bolts',
+            offset: 'ET45-56',
+            status: 'needs_review'
+          }
+        ]
+      },
+      {
+        name: 'A4',
+        slug: 'a4',
+        records: [
+          {
+            generation: 'B5 / B6 / B7',
+            years: '1995-2008',
+            market: 'EU',
+            pcd: '5x112',
+            centerBore: '57.1',
+            thread: 'M14x1.5',
+            fastener: 'bolts',
+            offset: 'ET35-45',
+            status: 'needs_review'
+          },
+          {
+            generation: 'B8 / B9',
+            years: '2008-2024',
+            market: 'EU',
+            pcd: '5x112',
+            centerBore: '66.6',
+            thread: 'M14x1.5',
+            fastener: 'bolts',
+            offset: 'ET35-50',
+            status: 'needs_review'
+          }
+        ]
+      }
+    ]
+  },
+  {
+    name: 'Volkswagen',
+    slug: 'volkswagen',
+    models: [
+      {
+        name: 'Golf',
+        slug: 'golf',
+        records: [
+          {
+            generation: 'Mk4',
+            years: '1997-2006',
+            market: 'EU',
+            pcd: '5x100',
+            centerBore: '57.1',
+            thread: 'M14x1.5',
+            fastener: 'bolts',
+            offset: 'ET35-42',
+            status: 'needs_review'
+          },
+          {
+            generation: 'Mk5 / Mk6 / Mk7 / Mk8',
+            years: '2003-2026',
+            market: 'EU',
+            pcd: '5x112',
+            centerBore: '57.1',
+            thread: 'M14x1.5',
+            fastener: 'bolts',
+            offset: 'ET45-51',
+            status: 'needs_review'
+          }
+        ]
+      }
+    ]
+  },
+  {
+    name: 'Toyota',
+    slug: 'toyota',
+    models: [
+      {
+        name: 'Corolla',
+        slug: 'corolla',
+        records: [
+          {
+            generation: 'E110-E210',
+            years: '1995-2026',
+            market: 'EU',
+            pcd: '5x100',
+            centerBore: '54.1',
+            thread: 'M12x1.5',
+            fastener: 'nuts',
+            offset: 'ET35-45',
+            status: 'needs_review'
+          }
+        ]
+      }
+    ]
+  }
+];
